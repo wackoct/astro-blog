f4917d6f5c63a90fd48c2fa30a0077d55907283f
wackoct <wackoct@163.com>
2022-09-08 12:26:27 +0800

    0908

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from 'astro/config';
+import svelte from '@astrojs/svelte';
+import mdx from '@astrojs/mdx';
+
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://astro-blog-template.netlify.app',
+  integrations: [mdx(), svelte()],
+  markdown: {
+    shikiConfig: {
+      theme: 'nord'
+    },
+    rehypePlugins: [['rehype-external-links', {
+      target: '_blank'
+    }]]
+  },
+  adapter: vercel()
+});
